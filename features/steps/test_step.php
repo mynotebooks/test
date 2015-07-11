a2044bca22d019fd52ebcb55875c052638b256b7
yusuke <hatooooooo@gmail.com>
2015-07-12 00:44:32 +0900

    no message

--- features/steps/test_step.php
+++ features/steps/test_step.php
@@ -3,7 +3,7 @@ use Behat\Behat\Context\Step\Given,
 	Behat\Behat\Context\Step\When,
 	Behat\Behat\Context\Step\Then;
 
-$steps->When('テストページ$/', function($world, $page) {
+$steps->When('/^テストページ$/', function($world, $page) {
 	return [
 		new When('"' . "/test.php" . '" へ移動する'),
 	];
